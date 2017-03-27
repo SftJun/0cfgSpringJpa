11781a03174bb09af378b603faaa8b8699de3818
YinJun <sftjun@outlook.com>
2017-03-27 21:51:21 +0800

    add page interface and impl

--- src/main/java/com/outlook/sftjun/db/UserService.java
+++ src/main/java/com/outlook/sftjun/db/UserService.java
@@ -3,6 +3,10 @@ package com.outlook.sftjun.db;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,5 +21,10 @@ public class UserService {
 	public User save(User user) {
 		log.debug("saved user");
 		return userRep.save(user);
+	}
+
+	public Page<User> findByPageAndSize(int page,int size){
+		Pageable pageable = new PageRequest(page, size);
+		return userRep.findAll(pageable);
 	}
 }
