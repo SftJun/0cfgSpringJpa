88a70ea2a032def9dacdfd3944a27150d5887275
YinJun <sftjun@outlook.com>
2017-03-27 21:51:55 +0800

    test page

--- src/test/java/com/outlook/sftjun/AppTest.java
+++ src/test/java/com/outlook/sftjun/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,6 +12,7 @@ import com.outlook.sftjun.cfg.AppConfig;
 import com.outlook.sftjun.cfg.PersistenceJPAConfig;
 import com.outlook.sftjun.db.User;
 import com.outlook.sftjun.db.UserRep;
+import com.outlook.sftjun.db.UserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class,PersistenceJPAConfig.class})
@@ -18,6 +20,9 @@ public class AppTest {
 	
 	@Autowired
 	private UserRep userRep;
+	
+	@Autowired
+	private UserService userService;
 	
 	private String userName = "UserName";
 	private String password = "password";
@@ -35,5 +40,11 @@ public class AppTest {
 	@Test
 	public void tstFind(){
 		Assert.assertEquals(password,userRep.findByUserName(userName).getPassword());
+	}
+	
+	@Test
+	public void tstPage(){
+		Page<User> users = userService.findByPageAndSize(1, 1);
+		users.getContent().forEach(user -> System.out.println(user.getUserName()));
 	}
 }
